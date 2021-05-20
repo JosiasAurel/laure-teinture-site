873902e262381617716be51f0180d963503015ac
Josias Aurel <ndjosiasaurel@gmail.com>
2021-05-20 05:51:07 +0100

    Update modal.jsx

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -26,15 +26,15 @@ const Modal = ({ open }) => {
             <div>
                 <span>
                     <Image src="/wa.png" width="50" height="50" />
-                    <h3>+237 677317027</h3>
+                    <h3>+237 683927562</h3>
                 </span>
                 <span>
                     <Image src="/phone.png" width="30" height="30" />
-                    <h3>+237 697174821</h3>
+                    <h3>+237 683927562</h3>
                 </span>
                 <span>
                     <Image src="/gmail.png" width="20" height="20" />
-                    <h3>sndemni@gmail.com</h3>
+                    <h3>hello@example.com</h3>
                 </span>
             </div>
         </div> }
